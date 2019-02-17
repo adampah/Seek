b8e817530614bf3989af884947c2a3339322c3e7
adam <adam@aquabyte.ai>
2019-02-16 20:32:58 -0800

    new stuff

---
+++ Seek/Discover.swift
@@ -0,0 +1,30 @@
+//
+//  Discover.swift
+//  Seek
+//
+//  Created by Adam Pahlavan on 2/16/19.
+//  Copyright © 2019 Treehacks. All rights reserved.
+//
+
+import AVFoundation
+import Foundation
+import UIKit
+//import HoundifySDK
+
+class Discover: UIViewController {
+    
+    //MARK: properties
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+}
+
--- Seek/ViewController.swift
+++ Seek/ViewController.swift
@@ -7,19 +7,27 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    //MARK: properties
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let string = "Hello, World!"
+        let utterance = AVSpeechUtterance(string: string)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+        let synth = AVSpeechSynthesizer()
+        synth.speak(utterance)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
